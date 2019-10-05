d421cf48ee4a37690db0f574c5c41b091a05a6d4
RodolphoCLobo <rodolpho1704@gmail.com>
2019-10-05 19:20:11 -0300

    Feat: factories and schema changes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191003023444) do
+ActiveRecord::Schema.define(version: 20191005215709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "user_id"
+    t.integer "priority_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "priorities", force: :cascade do |t|
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ spec/factories/notes.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :note do
+    title { "MyString" }
+    body { "MyString" }
+    priority { 1 }
+  end
+end
