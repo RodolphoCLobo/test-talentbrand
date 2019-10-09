3e31f897b81bfcf666eeb3ae9875487455586d9a
RodolphoCLobo <rodolpho1704@gmail.com>
2019-10-09 19:11:04 -0300

    Feat: add seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create({email: 'foo@bar.com', password:'password' password_confirmation: 'password'})
+['low' 'medium', 'high'].each { |status| Priority.create(status: status) }
+[
+  { user: User.first, priority: Priority.where(status: 'low').first, title: 'Test 1', body: 'This is test 1.' },
+  { user: User.first, priority: Priority.where(status: 'medium').first, title: 'Test 2', body: 'This is test 2.' },
+  { user: User.first, priority: Priority.where(status: 'high').first, title: 'Test 3', body: 'This is test 3.' },
+  { user: User.first, priority: Priority.where(status: 'low').first, title: 'Test 4', body: 'This is test 4.' },
+].each { |obj| Note.create(obj) }
