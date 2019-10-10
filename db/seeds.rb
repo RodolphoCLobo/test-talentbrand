2f91be0643dbe0c0603b924c0923a1cfb0ab7bd5
RodolphoCLobo <rodolpho1704@gmail.com>
2019-10-10 02:15:03 -0300

    Fix: improve seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
-User.create({email: 'foo@bar.com', password:'password', password_confirmation: 'password'})
-['low' 'medium', 'high'].each { |status| Priority.create(status: status) }
-[
-  { user: User.first, priority: Priority.where(status: 'low').first, title: 'Test 1', body: 'This is test 1.' },
-  { user: User.first, priority: Priority.where(status: 'medium').first, title: 'Test 2', body: 'This is test 2.' },
-  { user: User.first, priority: Priority.where(status: 'high').first, title: 'Test 3', body: 'This is test 3.' },
-  { user: User.first, priority: Priority.where(status: 'low').first, title: 'Test 4', body: 'This is test 4.' },
-].each { |obj| Note.create(obj) }
+User.delete_all
+Priority.delete_all
+Note.delete_all
+
+User.create(email: 'foo@bar.com', password:'password', password_confirmation: 'password')
+Priority.create(status: 'low')
+Priority.create(status: 'medium')
+Priority.create(status: 'high')
+Note.create(user: User.first, priority: Priority.where(status: 'low').first, title: 'Test tilte 1', body: 'This is test note 1.')
+Note.create(user: User.first, priority: Priority.where(status: 'medium').first, title: 'Test tilte 2', body: 'This is test note 2.')
+Note.create(user: User.first, priority: Priority.where(status: 'high').first, title: 'Test tilte 3', body: 'This is test note 3.')
+Note.create(user: User.first, priority: Priority.where(status: 'low').first, title: 'Test tilte 4', body: 'This is test note 4.')
